1e6f954ed5c58c3fe5180c470563849816da029f
Audrius <dr.audrius.m@gmail.com>
2018-07-10 14:20:16 +0300

    initial commit

---
+++ src/testPage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var Test = React.createClass({
+	render: function () {
+		return (
+			<div className="jumbotron">
+				<h1>test</h1>
+			</div>
+		);
+	}
+});
+
+module.exports = Test;
